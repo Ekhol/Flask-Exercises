df243f1da672a883cf1e38ca9bad4fec15e14b39
Nathan <frost.nathan1995@gmail.com>
2022-05-29 20:51:31 -0400

    finished math

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,37 @@
-# Put your app in here.
+from flask import Flask
+from flask import request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+@app.route('/math/add')
+def addition():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a, b)
+    return str(result)
+
+
+@app.route('/math/sub')
+def subtraction():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a, b)
+    return str(result)
+
+
+@app.route('/math/mult')
+def multiply():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = mult(a, b)
+    return str(result)
+
+
+@app.route('math/div')
+def divide():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = div(a, b)
+    return str(result)
